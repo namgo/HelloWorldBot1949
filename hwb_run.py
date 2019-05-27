e4918bf8dbb2c6d5e9b9caa90af360a433aa8d38
Nahum Del Toro <nahumdeltoro@riseup.net>
2019-05-26 23:40:05 -0400

    adding threading

--- hwb_run.py
+++ hwb_run.py
@@ -4,16 +4,19 @@ import glob
 import json
 import shlex
 from subprocess import call, TimeoutExpired
+import threading
+import queue
 
- # 4 seconds
-TIMEOUT = 4
+ # 20 seconds
+TIMEOUT = 20
 
+q = queue.Queue()
 os.chdir('/build')
 
 filenames = [filename.split('.')[0] for filename in glob.glob('*.asm')]
 print('processing {} files'.format(len(filenames)))
 
-for filename in filenames:
+def run_command(filename):
     cmd = 'gdb {0} "set logging file /build/{0}.output" -ex "set logging on" -ex r -ex "bt full" -ex "info registers" -ex quit'.format(filename)
     cmd = shlex.shlex(cmd)
     data = {
@@ -26,3 +29,27 @@ for filename in filenames:
         data['timeout_hit'] = True
     with open(filename+'.timeout.json', 'w') as f:
         f.write(json.dumps(data))
+
+def worker():
+    while True:
+        item = q.get()
+        if item is None:
+            break
+        run_command(item)
+        q.task_done()
+
+threads = []
+for i in range(10):
+    t = threading.Thread(target=worker)
+    t.start()
+    threads.append(t)
+
+for filename in filenames:
+    q.put(filename)
+
+q.join()
+
+for i in range(10):
+    q.put(None)
+for t in threads:
+    t.join()
