44d56ff060bf14be222f05b658cbb02635a9cc8b
Nahum Del Toro <nahumdeltoro@riseup.net>
2019-05-26 22:10:54 -0400

    order matters?

--- run_docker.sh
+++ run_docker.sh
@@ -5,9 +5,9 @@ docker build -t nasm_builder .
 
 echo "building and running assembly files"
 docker run -dt nasm_builder:latest \
-       --cap-add=SYS_PTRACE \
-       --security-opt seccomp=unconfined \
        -v `pwd`:/opt/,ro \
        -v `pwd`/output:/build,rw \
+       --cap-add=SYS_PTRACE \
+       --security-opt seccomp=unconfined \
        /bin/bash /opt/build_and_run.sh
 
