0e5b13d158fd9faadf36b74331cca47ebb77ebc4
Nahum Del Toro <nahumdeltoro@riseup.net>
2019-05-26 20:26:52 -0400

    gdb is confusing me

--- hwp_run.sh
+++ hwp_run.sh
@@ -6,7 +6,7 @@ cd $working_dir
 for f_asm in `ls *.asm`
 do 
     executable_name=`echo $f_asm | cut -d'.' -f1`
-    gdb_cmd='gdb -batch -ex "set logging on" -ex "set logging file '$executable_name'.output" -ex r -ex "bt full" -ex "info registers" -ex quit'
+    gdb_cmd='gdb -batch -ex "set logging on" -ex "set logging file /build/'$executable_name'.output" -ex r -ex "bt full" -ex "info registers" -ex quit'
     `$gdb_cmd $executable_name`
 done
 cd /opt
