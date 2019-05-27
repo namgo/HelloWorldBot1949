caa4fe6903c40e9cac075a765ac4d3af5a6a87fa
Nahum Del Toro <nahumdeltoro@riseup.net>
2019-05-26 22:19:34 -0400

    :angery:

--- run_docker.sh
+++ run_docker.sh
@@ -4,4 +4,4 @@ echo "building docker image"
 docker build -t nasm_builder .
 
 echo "building and running assembly files"
-docker run -dt -v `pwd`:/opt/,ro -v `pwd`/output:/build,rw --cap-add=SYS_PTRACE --security-opt seccomp=unconfined nasm_builder:latest /bin/bash /opt/build_and_run.sh
+docker run -dt -v `pwd`:/opt/ -v `pwd`/output:/build --cap-add=SYS_PTRACE --security-opt seccomp=unconfined nasm_builder:latest /bin/bash /opt/build_and_run.sh
