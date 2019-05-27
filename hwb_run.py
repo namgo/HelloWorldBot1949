9b276b085a69bcced973017d8b620f33c911a62d
Nahum Del Toro <nahumdeltoro@riseup.net>
2019-05-26 23:21:41 -0400

    need to add shlex

--- hwb_run.py
+++ hwb_run.py
@@ -2,6 +2,7 @@
 import os
 import glob
 import json
+import shlex
 from subprocess import call, TimeoutExpired
 
  # 4 seconds
@@ -14,6 +15,7 @@ print('processing {} files'.format(len(filenames)))
 
 for filename in filenames:
     cmd = ['gdb {0} "set logging file /build/{0}.output" -ex "set logging on" -ex r -ex "bt full" -ex "info registers" -ex quit'.format(filename)]
+    cmd = shlex.shlex(cmd)
     data = {
         'timeout_time': TIMEOUT,
         'timeout_hit': False
