385470793ef0072087c942be290d4420e1214fa5
Nahum Del Toro <nahumdeltoro@riseup.net>
2019-05-26 20:31:20 -0400

    debugging my debug script

--- hwb_run.sh
+++ hwb_run.sh
@@ -7,6 +7,7 @@ for f_asm in `ls *.asm`
 do 
     executable_name=`echo $f_asm | cut -d'.' -f1`
     gdb_cmd='gdb -batch -ex "set logging on" -ex "set logging file /build/'$executable_name'.output" -ex r -ex "bt full" -ex "info registers" -ex quit'
+    echo $gdb_cmd
     `$gdb_cmd $executable_name`
 done
 cd /opt
