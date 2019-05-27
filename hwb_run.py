94cdd3202eadc00f83d0e80f0936afef11a31afa
Nahum Del Toro <nahumdeltoro@riseup.net>
2019-05-26 23:18:38 -0400

    adding the timeout function with python

---
+++ hwb_run.py
@@ -0,0 +1,26 @@
+# multithreaded run with timeout
+import os
+import glob
+import json
+from subprocess import call, TimeoutExpired
+
+ # 4 seconds
+TIMEOUT = 4
+
+os.chdir('/build')
+
+filenames = [filename.split('.')[0] for filename in glob.glob('*.asm')]
+print('processing {} files'.format(len(filenames)))
+
+for filename in filenames:
+    cmd = ['gdb {0} "set logging file /build/{0}.output" -ex "set logging on" -ex r -ex "bt full" -ex "info registers" -ex quit'.format(filename)]
+    data = {
+        'timeout_time': TIMEOUT,
+        'timeout_hit': False
+    }
+    try:
+        call(cmd, timeout=TIMEOUT)
+    except TimeoutExpired:
+        data['timeout_hit'] = True
+    with open(filename+'.timeout.json', 'w') as f:
+        f.write(json.dumps(data))
