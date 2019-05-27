487bfd18f1d75d63eeedd52c97074fc6aad640a6
Nahum Del Toro <nahumdeltoro@riseup.net>
2019-05-26 22:15:06 -0400

    I messed something up and I don't know what

--- run_docker.sh
+++ run_docker.sh
@@ -4,10 +4,4 @@ echo "building docker image"
 docker build -t nasm_builder .
 
 echo "building and running assembly files"
-docker run -dt nasm_builder:latest \
-       -v `pwd`:/opt/,ro \
-       -v `pwd`/output:/build,rw \
-       --cap-add=SYS_PTRACE \
-       --security-opt seccomp=unconfined \
-       /bin/bash /opt/build_and_run.sh
-
+docker run -it -v `pwd`:/opt/,ro -v `pwd`/output:/build,rw --cap-add=SYS_PTRACE --security-opt seccomp=unconfined nasm_builder:latest /bin/bash
