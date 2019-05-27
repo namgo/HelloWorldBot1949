e7155625135c28ea1c04bdf14ec14a664543dff4
Nahum Del Toro <nahumdeltoro@riseup.net>
2019-05-26 22:29:02 -0400

    why do I do this to myself?

--- hwb_build.sh
+++ hwb_build.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-output_dir='/build'
-build='nasm -f elf64 -F dwarf -g'
-link='ld -o'
 # $gdb_cmd = 'gdb a.out -batch -ex "set logging on" -ex r -ex "bt full" -ex "info registers" -ex quit'
 
 cd $output_dir
 for f_asm in `ls *.asm`
 do 
-    generic_name=`echo $f_asm | cut -d'.' -f1`
-    `$build $f_asm`
-    `$link $generic_name $generic_name.o`
-    `rm $generic_name.o`
+    generic_name=$(echo $f_asm | cut -d'.' -f1)
+    nasm -f elf64 -F dwarf -g $f_asm
+    ld -o $generic_name $generic_name.o
+    rm $generic_name.o
 done
 cd /opt
