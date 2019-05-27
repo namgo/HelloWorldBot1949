169ea94edeafafc2c6bc8d0ed0343a9a5ad841d3
Nahum Del Toro <nahumdeltoro@riseup.net>
2019-05-26 22:15:53 -0400

    hey, it's working

--- run_docker.sh
+++ run_docker.sh
@@ -4,4 +4,4 @@ echo "building docker image"
 docker build -t nasm_builder .
 
 echo "building and running assembly files"
-docker run -it -v `pwd`:/opt/,ro -v `pwd`/output:/build,rw --cap-add=SYS_PTRACE --security-opt seccomp=unconfined nasm_builder:latest /bin/bash
+docker run -dt -v `pwd`:/opt/,ro -v `pwd`/output:/build,rw --cap-add=SYS_PTRACE --security-opt seccomp=unconfined nasm_builder:latest /bin/bash /opt/build_and_run.sh
