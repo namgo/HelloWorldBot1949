e16c0f9eea91d1bcb0540c6248b4aa716e56aa40
Nahum Del Toro <nahumdeltoro@riseup.net>
2019-05-26 20:37:17 -0400

    I don't know anything

--- hwb_run.sh
+++ hwb_run.sh
@@ -5,7 +5,7 @@ working_dir='/build'
 cd $working_dir
 for f_asm in `ls *.asm`
 do 
-    executable_name=
-    gdb `echo $f_asm | cut -d'.' -f1` -batch -ex "set logging file /build/$executable_name.output" -ex r -ex "bt full" -ex "info registers" -ex quit
+    executable_name=`echo $f_asm | cut -d'.' -f1`
+    gdb $executable_name -batch -ex "set logging file /build/$executable_name.output" -ex r -ex "bt full" -ex "info registers" -ex quit
 done
 cd /opt
