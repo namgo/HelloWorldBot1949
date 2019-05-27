3cda3a3c659a1c1de45fe26c7919f250a3434996
Nahum Del Toro <nahumdeltoro@riseup.net>
2019-05-26 20:33:57 -0400

    I don't know how docker works

--- hwb_run.sh
+++ hwb_run.sh
@@ -6,8 +6,6 @@ cd $working_dir
 for f_asm in `ls *.asm`
 do 
     executable_name=`echo $f_asm | cut -d'.' -f1`
-    gdb_cmd='gdb -batch -ex "set logging on" -ex "set logging file /build/'$executable_name'.output" -ex r -ex "bt full" -ex "info registers" -ex quit'
-    echo $gdb_cmd
-    `$gdb_cmd $executable_name`
+    `gdb '$executable_name' -batch -ex "set logging file /build/'$executable_name'.output" -ex r -ex "bt full" -ex "info registers" -ex quit`
 done
 cd /opt
