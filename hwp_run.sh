0e8d4254c07562dae1f0eb3e04b02d4ce5ad4b25
Nahum Del Toro <nahumdeltoro@riseup.net>
2019-05-26 20:24:49 -0400

    a bit messy but this should work

--- hwp_run.sh
+++ hwp_run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 working_dir='/build'
-gdb_cmd='gdb -batch -ex "set logging on" -ex r -ex "bt full" -ex "info registers" -ex quit'
 
 cd $working_dir
 for f_asm in `ls *.asm`
 do 
     executable_name=`echo $f_asm | cut -d'.' -f1`
-    `$gdb_cmd $executable_name > $executable_name.output`
+    gdb_cmd='gdb -batch -ex "set logging on" -ex "set logging file '$executable_name'.output" -ex r -ex "bt full" -ex "info registers" -ex quit'
+    `$gdb_cmd $executable_name`
 done
 cd /opt
