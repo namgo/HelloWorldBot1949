9338f1ebbe3a91851d682cdb2636baa116bf0356
Nahum Del Toro <nahumdeltoro@riseup.net>
2019-05-26 22:29:53 -0400

    I should actually debug this

--- hwb_build.sh
+++ hwb_build.sh
@@ -5,8 +5,10 @@
 cd $output_dir
 for f_asm in `ls *.asm`
 do 
+    echo "built $f_asm"
     generic_name=$(echo $f_asm | cut -d'.' -f1)
     nasm -f elf64 -F dwarf -g $f_asm
+    echo "linked $f_asm"
     ld -o $generic_name $generic_name.o
     rm $generic_name.o
 done
