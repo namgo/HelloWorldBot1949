da348088db113453b6ba8f62568f03a6725db640
Nahum Del Toro <nahumdeltoro@riseup.net>
2019-05-26 22:08:00 -0400

    adding equals sign

--- run_docker.sh
+++ run_docker.sh
@@ -5,7 +5,7 @@ docker build -t nasm_builder .
 
 echo "building and running assembly files"
 docker run -dt nasm_builder:latest \
-       --cap-add SYS_PTRACE \
+       --cap-add=SYS_PTRACE \
        --security-opt seccomp=unconfined \
        -v `pwd`:/opt/,ro \
        -v `pwd`/output:/build,rw \
